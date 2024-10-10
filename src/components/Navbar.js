43bce96256f314079c19e2cf2e5c86af48689bdd
SERajamani <rajamanise21@gmail.com>
2024-10-10 11:29:03 +0530

    update perfumy-raect project

---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+// Navbar Component
+
+function Navbar(){
+    return (
+      <nav>
+        <h1>Perfume</h1>
+        <ul>
+          <li>Home |</li>
+          <li>Product |</li>
+          <li>About |</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    )
+  }
+
+  export default Navbar
